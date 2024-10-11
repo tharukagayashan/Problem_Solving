eb8c8e9dd2032abf45a1b7ffa0ac1c2c85fe4c56
Tharuka Gayashan <tharuka.gayashan.5@gmail.com>
2024-10-11 17:13:00 +0530

    MiniMaxSum

---
+++ MinMaxSum.java
@@ -0,0 +1,42 @@
+import java.io.*;
+import java.util.*;
+
+class MiniMaxSum {
+
+    public static void miniMaxSum(List<Integer> arr) {
+        long max = 0;
+        long min = 0;
+        long sum = 0;
+        for (Integer i : arr) {
+            sum += i;
+            if (i >= max) {
+                max = i;
+            }
+
+            if (min == 0L && i > min) {
+                min = i;
+            } else if (i < min) {
+                min = i;
+            }
+        }
+
+        System.out.println((sum - max) + " " + (sum - min));
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] arrTemp = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        List<Integer> arr = new ArrayList<>();
+
+        for (int i = 0; i < 5; i++) {
+            int arrItem = Integer.parseInt(arrTemp[i]);
+            arr.add(arrItem);
+        }
+
+        MiniMaxSum.miniMaxSum(arr);
+
+        bufferedReader.close();
+    }
+}
